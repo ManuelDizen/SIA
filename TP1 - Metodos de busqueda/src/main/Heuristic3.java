095c7a10bfe3bb5a7a73e7fe1dd95219581b38fc
desimonef <67933006+desimonef@users.noreply.github.com>
2022-03-14 19:24:31 -0300

    Update Heuristic3.java

--- TP1 - Metodos de busqueda/src/main/Heuristic3.java
+++ TP1 - Metodos de busqueda/src/main/Heuristic3.java
@@ -11,7 +11,7 @@ public class Heuristic3 implements Heuristic{
 
     public int getHValue(State s){
         int firstDisc = calculateDiscs(n);
-        int HValue = (int) Math.pow(2, firstDisc-1)-1;
+        int HValue = (int) Math.pow(2, firstDisc-2);
         for(int i=firstDisc; i<7; i++) {
             HValue += Math.pow(2, i+1) - 2;
         }
