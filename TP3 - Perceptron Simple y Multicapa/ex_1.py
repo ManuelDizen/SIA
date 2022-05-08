37ae026ed514b2cb71e8c560a4c11b3a1210e305
Manuel <mdizenhaus@itba.edu.ar>
2022-05-08 13:49:20 -0300

    Ej 1 con gráficos

--- TP3 - Perceptron Simple y Multicapa/ex_1.py
+++ TP3 - Perceptron Simple y Multicapa/ex_1.py
@@ -7,7 +7,7 @@ from src.methods import *
 print("TP3 - Perceptron simple y multicapa\n")
 print("Ejercicio 1: Perceptron siple con función de activación escalón con dos funciones:")
 
-trainDataAnd = numpy.array([[-1,1],[1,-1],[-1,-1],[1,1]])
+trainDataAnd = numpy.array([[-1,1,1],[1,-1,1],[-1,-1,1],[1,1,1]])
 expectedOutputAnd = numpy.array([-1,-1,-1,1])
 
 # col = numpy.where(expectedOutputAnd < 1,'r','b')
@@ -19,9 +19,9 @@ expectedOutputAnd = numpy.array([-1,-1,-1,1])
 # plt.grid()
 # plt.show()
 # plt.savefig('graphs/puntos_and.png')
-print("\nFunción lógica \"XOR\"")
+#print("\nFunción lógica \"XOR\"")
 #
-trainDataXor = numpy.array([[-1,1],[1,-1],[-1,-1],[1,1]])
+trainDataXor = numpy.array([[-1,1,1],[1,-1,1],[-1,-1,1],[1,1,1]])
 expectedOutputXor = numpy.array([1,1,-1,-1])
 
 # col = numpy.where(expectedOutputXor < 1,'r','b')
@@ -33,10 +33,55 @@ expectedOutputXor = numpy.array([1,1,-1,-1])
 # plt.grid()
 # plt.show()
 # plt.savefig('graphs/puntos_xor.png')
-#perceptron2 = perceptron(trainDataXor, expectedOutputXor, 0.1, simpleActivationFunc, simpleErrorFunc, True)
-#perceptron2.algorithm(10)
-
-print("\nFunción lógica \"AND\"")
+# print("\nFunción lógica \"XOR\"")
 #
-perceptron = perceptron(trainDataAnd, expectedOutputAnd, 0.1, simpleActivationFunc, simpleErrorFunc, True)
+# perceptron2 = perceptron(trainDataXor, expectedOutputXor, 0.1, simpleActivationFunc, simpleErrorFunc, True)
+# perceptron.algorithm2(30)
+
+# print("\nFunción lógica \"AND\"")
+# #
+# perceptron = perceptron(trainDataAnd, expectedOutputAnd, 0.1, simpleActivationFunc, simpleErrorFunc, True)
+# perceptron.algorithm2(50)
+# print(f'W mínimo = {perceptron.w_min}, Error mínimo = {perceptron.error_min}')
+# x_axis_values = [-2, -1, 0, 1, 2]
+# y_axis_values = []
+# aux = perceptron.w_min*1.0
+# print(f'{aux}')
+# for i in range(len(x_axis_values)):
+#     t = (aux[1] * 1.0 * x_axis_values[i] + aux[2]) / -aux[0]
+#     y_axis_values.append(t)
+# ax = plt.subplot()
+# col = numpy.where(expectedOutputAnd < 1,'r','b')
+# plt.scatter([row[0] for row in trainDataAnd], [row[1] for row in trainDataAnd], c=col, s=300)
+# plt.title("Resultados de la función \"AND\"")
+# plt.xlabel("X")
+# plt.ylabel("Y")
+# plt.grid(visible=True)
+# ax.plot(x_axis_values, y_axis_values)
+# fig1 = plt.gcf()
+# plt.show()
+# plt.draw()
+
+print("\nFunción lógica \"XOR\"")
+#
+perceptron = perceptron(trainDataXor, expectedOutputXor, 0.1, simpleActivationFunc, simpleErrorFunc, True)
 perceptron.algorithm2(50)
+print(f'W mínimo = {perceptron.w_min}, Error mínimo = {perceptron.error_min}')
+x_axis_values = [-2, -1, 0, 1, 2]
+y_axis_values = []
+aux = perceptron.w_min*1.0
+print(f'{aux}')
+for i in range(len(x_axis_values)):
+    t = (aux[1] * 1.0 * x_axis_values[i] + aux[2]) / -aux[0]
+    y_axis_values.append(t)
+ax = plt.subplot()
+col = numpy.where(expectedOutputAnd < 1,'r','b')
+plt.scatter([row[0] for row in trainDataXor], [row[1] for row in trainDataXor], c=col, s=300)
+plt.title("Resultados de la función \"XOR\"")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.grid(visible=True)
+ax.plot(x_axis_values, y_axis_values)
+fig1 = plt.gcf()
+plt.show()
+plt.draw()
--- TP3 - Perceptron Simple y Multicapa/src/perceptron.py
+++ TP3 - Perceptron Simple y Multicapa/src/perceptron.py
@@ -4,7 +4,7 @@ import numpy
 class perceptron:
     def __init__(self, trainingData, expectedOutput, learnRate, activationFunc, errorFunc, isLinear=False, derivative=None, default_min_error=0.01):
         self.w_min = numpy.zeros(len(trainingData[0]))
-        self.error_min = (2.0*len(trainingData))
+        self.error_min = numpy.inf
         self.trainingData = trainingData
         self.expectedOutput = expectedOutput
         self.learnRate = learnRate
@@ -42,12 +42,14 @@ class perceptron:
             i = i + 1
         print(f'Iters: {i}, Min error: {self.error_min}, Min w: {self.w_min}')
 
-    def algorithm2(self, maxIterations):
+    def algorithm2(self, maxIterations=30):
         i = 0
-        w = numpy.zeros(len(self.trainingData[0]))
+        w = numpy.zeros(len(self.trainingData[0]) - 1)
+        w = numpy.append(w, 1)
         error = 1
         while error > self.default_min_error and i < maxIterations:
             idx = numpy.random.randint(0, len(self.trainingData) - 1)
+            #print(f'Len de w = {len(w)} ({w}) y Len de training = {len(self.trainingData[idx])} ({self.trainingData[idx]})')
             h_value = numpy.dot(self.trainingData[idx], w)
             activation = self.activationFunc(h_value)
             delta_w = self.learnRate * (self.expectedOutput[idx] - activation) * self.trainingData[idx]
@@ -59,10 +61,10 @@ class perceptron:
             #     if self.isLinear is not True:
             #         delta_w *= self.derivative(activation)
             #     w[j] += delta_w
-            error = self.errorFunc(self.trainingData, self.expectedOutput, self.w_min, self.activationFunc)
+            error = self.errorFunc(self.trainingData, self.expectedOutput, w, self.activationFunc)
             if error < self.error_min:
                 print(f'(Ya estoy cambiando) Error = {error}, self.error_min = {self.error_min}')
                 self.error_min = error
                 self.w_min = w
             i = i+1
-            print(f'h = {activation}, expectedOut = {self.expectedOutput[idx]}, w = {self.w_min[0]} {self.w_min[1]}, error = {self.error_min}\n')
+        print(f'w = {w} error = {self.error_min}\n')
