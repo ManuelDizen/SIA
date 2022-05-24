0bd649d976688949ced90aecbcbaefa033eae92b
Manuel <mdizenhaus@itba.edu.ar>
2022-05-24 18:34:58 -0300

    En realidad simplemente converge a lo mismo pero con signos invertidos

--- TP4 - Métodos de Aprendizaje NO supervisado/ej1.2.py
+++ TP4 - Métodos de Aprendizaje NO supervisado/ej1.2.py
@@ -10,24 +10,27 @@ labels = df[df.columns[0]].to_numpy()
 inputs = df.loc[:, ["Area", "GDP", "Inflation", "Life.expect", "Military", "Pop.growth", "Unemployment"]].values
 inputs = StandardScaler().fit_transform(inputs)
 
-learnRate = 0.1
-#w = np.zeros(len(inputs[0]))
-w = np.random.rand(len(inputs[0]))
-print(f'w inicial = {w}')
-nOfIterations = 500
+#w_inicial = np.random.rand(len(inputs[0]))
 
-outputWs = np.empty(shape=(nOfIterations, len(inputs[0])))
-#print(f'outputWs inicial = {outputWs}')
-for iter in range(0, nOfIterations):
-    for i in range(0, len(inputs)):
-        s = 0
-        for j in range(0, len(inputs[i])):
-            s += inputs[i][j] * w[j]
-        #print(f'Previous w: {w}')
-        w = w + learnRate*s*(inputs[i] - s*w)
-        #print(f'New w= {w}, Delta_w={learnRate*s*(inputs[i] - s*w)}')
-    outputWs[iter] = w
+for i in range(0,10):
+    learnRate = 0.1
+    #w = np.zeros(len(inputs[0]))
+    w = np.random.rand(len(inputs[0]))
+    #print(f'w inicial = {w}')
+    nOfIterations = 500
 
-print(f'{outputWs[-1]}\nshape={outputWs.shape}')
-#for i in range(0, len(inputs[0])):
-#    print(f'Promedio de variable X_{i} ({desired[i]}) = {outputWs[:,i].mean()}\n')
+    outputWs = np.empty(shape=(nOfIterations, len(inputs[0])))
+    #print(f'outputWs inicial = {outputWs}')
+    for iter in range(0, nOfIterations):
+        for i in range(0, len(inputs)):
+            s = 0
+            for j in range(0, len(inputs[i])):
+                s += inputs[i][j] * w[j]
+            #print(f'Previous w: {w}')
+            w = w + learnRate*s*(inputs[i] - s*w)
+            #print(f'New w= {w}, Delta_w={learnRate*s*(inputs[i] - s*w)}')
+        outputWs[iter] = w
+
+    print(f'{outputWs[-1]}')
+    #for i in range(0, len(inputs[0])):
+    #    print(f'Promedio de variable X_{i} ({desired[i]}) = {outputWs[:,i].mean()}\n')
