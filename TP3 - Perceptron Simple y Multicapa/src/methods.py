0e127186131094815a2fad93e7d464cfc02c61cb
desimonef <67933006+desimonef@users.noreply.github.com>
2022-05-23 23:34:18 -0300

    Update methods.py

--- TP3 - Perceptron Simple y Multicapa/src/methods.py
+++ TP3 - Perceptron Simple y Multicapa/src/methods.py
@@ -1,6 +1,8 @@
 import numpy
 
-betaParameter = 0.5
+betaParameter = 1
+maxVal = 99.3834
+minVal = 0.1558
 
 def simpleActivationFunc(value):
     return 1 if value >= 0 else -1
@@ -24,3 +26,37 @@ def simpleErrorFunc(trainingData, expectedOutput, w, activationFunc):
         actualOut = numpy.dot(trainingData[i], w)
         error += ((expectedOutput[i] - activationFunc(actualOut)) ** 2)
     return error / 2
+
+def denormalizeErrorFunc(trainingData, expectedOutput, w, activationFunc):
+    limit = len(trainingData)
+
+
+    error = 0
+    for i in range(limit):
+        aux = activationFunc(numpy.dot(trainingData[i], w))
+        actualOut = ((aux + 1)*(maxVal-minVal)/2)+minVal
+        expected = ((expectedOutput[i]+1)*(maxVal-minVal)/2)+minVal
+        error += ((expected - actualOut) ** 2)
+    return error / 2
+
+def normalize(output):
+    outData = numpy.zeros(len(output))
+
+    for i in range(len(output)):
+        current = (2*(output[i] - minVal)/(maxVal - minVal))-1
+        outData[i] = current
+
+    return outData
+
+def calcAccuracy(data, output, w, activationFunc):
+    count = 0
+    delta = 0.25
+    for i in range(len(data)):
+        aux = activationFunc(numpy.dot(data[i], w))
+        current = ((aux + 1)*(maxVal-minVal)/2)+minVal
+        #  print(f'current = {current}, output = {output[i]}')
+        if abs(output[i] - current) < delta:
+            count += 1
+
+
+    return count/len(data)
