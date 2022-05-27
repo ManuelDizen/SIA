7f895e63c94e60f535b1b315107b93f7fef12a37
desimonef <67933006+desimonef@users.noreply.github.com>
2022-05-27 19:11:31 -0300

    Update plotting.py

--- TP4 - Métodos de Aprendizaje NO supervisado/plotting.py
+++ TP4 - Métodos de Aprendizaje NO supervisado/plotting.py
@@ -5,8 +5,12 @@ import matplotlib.pylab as plt
 plt.style.use("seaborn")
 
 
-def print_heatmap(matrix):
+def print_heatmap(matrix, names=None):
+
+    sns.set(font_scale=0.7)
     plt.figure(figsize=(len(matrix), len(matrix[0])))
-    heat_map = sns.heatmap(matrix, linewidth=1, annot=True)
+    heat_map = sns.heatmap(matrix, annot=names, linewidth=1, fmt="", cmap='RdYlGn')
+
     plt.title("HeatMap using Seaborn Method")
+
     plt.show()
