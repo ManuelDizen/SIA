1a7bc262fe4a2367eddcde9b853ef1d932806548
Mili Cornidez <mcornidez@itba.edu.ar>
2022-06-20 23:09:29 -0300

    add main ej2

---
+++ TP5 - Deep Learning/ej2.py
@@ -0,0 +1,18 @@
+from tensorflow import keras
+
+from src.variational_autoencoder import VariationalAutoencoder
+from src.plotting import *
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+trainset = np.concatenate([x_train, x_test], axis=0)
+mnist_digits = np.expand_dims(trainset, -1).astype("float32") / 255
+
+vae = VariationalAutoencoder()
+vae.compile(optimizer=keras.optimizers.Adam())
+vae.fit(mnist_digits, epochs=1, batch_size=128)
+
+plot_latent_space(vae)
+
+x_train = np.expand_dims(x_train, -1).astype("float32") / 255
+
+plot_label_clusters(vae, x_train, y_train)
